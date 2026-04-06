491ce2a2deffa41af3cf0278a06c1ea3760242cb
nelson.bidault-ferre2.auditeur <nelson.bidault-ferre2.auditeur@lecnam.net>
2026-04-06 16:01:32 +0200

    Push doors

--- Assets/Script/DoorScript.cs
+++ Assets/Script/DoorScript.cs
@@ -5,34 +5,27 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour
 {
     Animator animator;
+    private AudioSource audioSource;
+    public AudioClip openSound;
 
-    // Start is called before the first frame update
     void Start()
     {
-        //Assignation de son propre animator en tant que variable pour pouvoir y accéder plus simplement
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
-    //déclence l'animation d'ouverture des portes
-    //Y intégrer le jeu d'un son ? Le lancement d'une corroutine ?
     private void OnTriggerEnter(Collider other)
     {
         animator.SetBool("In", true);
+
+        if (audioSource != null && openSound != null)
+        {
+            audioSource.PlayOneShot(openSound);
+        }
     }
 
-    //déclence l'animation de fermeture des portes
-    //Y intégrer le jeu d'un son ? Le lancement d'une corroutine ?
     private void OnTriggerExit(Collider other)
     {
         animator.SetBool("In", false);
     }
-
-    //Créer une fonction publique à appeler lors d'un animation event ?
-
 }
